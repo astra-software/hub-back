8568e0bb7b3cf943737854fe836b47ad31ea0577
sntpwbl <joaop401@outlook.com>
2025-02-04 14:18:16 -0300

    feat: adicionando teste para validar tabela de feedback

---
+++ src/test/java/com/astra/api/hub_api/database/DatabaseSchemaTest.java
@@ -0,0 +1,55 @@
+package com.astra.api.hub_api.database;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@Testcontainers
+@SpringBootTest
+public class DatabaseSchemaTest {
+
+    @SuppressWarnings("resource")
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+        .withDatabaseName("hub_db")
+        .withUsername("test")
+        .withPassword("test");
+
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testUserTableExists() throws Exception{
+        try (Connection connection = dataSource.getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(
+                     "SELECT table_name FROM information_schema.tables WHERE table_name = 'feedback'")) {
+            assertTrue(rs.next(), "Tabela 'feedback' deveria existir.");
+        }
+    }
+
+
+}
