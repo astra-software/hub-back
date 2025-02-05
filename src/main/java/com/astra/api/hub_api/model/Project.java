c6bc4917685f15ab4d17dec7cc823ede8a7bfef6
sntpwbl <joaop401@outlook.com>
2025-02-05 18:18:23 -0300

    feat: adicionando model projeto, utils

---
+++ src/main/java/com/astra/api/hub_api/model/Project.java
@@ -0,0 +1,68 @@
+package com.astra.api.hub_api.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "project")
+public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Column
+  private List<User> authors;
+
+  @Column
+  private String title;
+
+  @Column
+  private String description;
+
+  @Column(name = "image_list")
+  private List<String> imageList;
+
+  @OneToMany(mappedBy = "project", fetch = FetchType.EAGER, orphanRemoval = true, cascade = CascadeType.ALL )
+  private List<Util> utils;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "project_category_link",
+              joinColumns = {@JoinColumn(name = "id_project")},
+              inverseJoinColumns = {@JoinColumn(name = "id_category")})
+  private List<Category> categories = new ArrayList<>();
+
+  //private List<Feedback> feedbacks;
+
+  @Column(name = "favourite_counter")
+  private Long favouriteCounter;
+
+  @Column
+  private LocalDateTime timestamp;
+
+}
---
+++ src/main/java/com/astra/api/hub_api/model/Util.java
@@ -0,0 +1,37 @@
+package com.astra.api.hub_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "util")
+public class Util {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String title;
+
+  @Column
+  private String content;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "project_id")
+  private Project project;
+
+}
