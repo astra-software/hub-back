6dc9b292068929b88798ab538be5b33bc6d24259
sntpwbl <joaop401@outlook.com>
2025-02-05 23:03:38 -0300

    fix: corrigindo import pra repositorio de categorias

--- src/main/java/com/astra/api/hub_api/repository/CategoryRepository.java
+++ src/main/java/com/astra/api/hub_api/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.astra.api.hub_api.repository;
 
-import java.util.Locale.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.astra.api.hub_api.model.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
