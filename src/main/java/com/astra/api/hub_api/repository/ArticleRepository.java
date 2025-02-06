43ec5960c5ae0450857dacb8411c7ee3233c445f
sntpwbl <joaop401@outlook.com>
2025-02-05 22:45:57 -0300

    feat: adicionando repositorio para entidades criadas

---
+++ src/main/java/com/astra/api/hub_api/repository/ArticleRepository.java
@@ -0,0 +1,11 @@
+package com.astra.api.hub_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.astra.api.hub_api.model.Article;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
